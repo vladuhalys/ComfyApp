80d4e88c018e66cfade713851293c0c4ff63acee
vladuhalys <vlad.lysenko1999@gmail.com>
2023-03-28 18:34:55 +0300

    Initial commit

---
+++ application.cpp
@@ -0,0 +1,10 @@
+#include "library.h"
+
+RUN_APP
+{
+	ukr_lang;
+	access_message("Access granted");
+	warning_message("Warning");
+	error_message("Error");
+	return 0;
+}
--- library.h
+++ library.h
@@ -1,1 +1,35 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+#define ukr_lang { SetConsoleCP(1251); SetConsoleOutputCP(1251); }
+#define print(val) { cout << val; }
+#define println(val) { cout << val << endl; }
+#define next_line { cout << endl; }
+#define RUN_APP int main()
+#define RED 12
+#define GREEN 10
+#define YELLOW 14
+#define DEFAULT 7
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+#define error_message(message) { \
+SetConsoleTextAttribute(hConsole, RED);\
+cout << message << endl;\
+SetConsoleTextAttribute(hConsole, DEFAULT);}
+
+#define access_message(message) { \
+SetConsoleTextAttribute(hConsole, GREEN);\
+cout << message << endl;\
+SetConsoleTextAttribute(hConsole, DEFAULT);}
+
+#define warning_message(message) { \
+SetConsoleTextAttribute(hConsole, YELLOW);\
+cout << message << endl;\
+SetConsoleTextAttribute(hConsole, DEFAULT);}
